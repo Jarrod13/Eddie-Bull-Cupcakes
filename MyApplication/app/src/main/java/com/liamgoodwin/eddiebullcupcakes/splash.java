e38711044ee394b213f1240fa80450830e0247e2
Liam Goodwin <goodwin_liam1@stclairconnect.ca>
2016-11-29 21:56:34 -0500

    Fixing an error where the app crashes after the loading screen is displayed

--- MyApplication/app/src/main/java/com/liamgoodwin/eddiebullcupcakes/splash.java
+++ MyApplication/app/src/main/java/com/liamgoodwin/eddiebullcupcakes/splash.java
@@ -1,7 +1,49 @@
 package com.liamgoodwin.eddiebullcupcakes;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 /**
  * Created by liamgoodwin on 16-11-29.
  */
-public class splash {
+public class splash extends Activity{
+
+    private static final int SPLASH_SHOW_TIME = 4800;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash);
+        new BackgroundSplashTask().execute();
+    }
+
+    private class BackgroundSplashTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+                Thread.sleep(SPLASH_SHOW_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            Intent i = new Intent(splash.this, MainActivity.class);
+            startActivity(i); finish();
+        }
+
+    }
 }
